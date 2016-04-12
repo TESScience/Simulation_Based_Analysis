0c5209f92c57b4e1e184230a93a8a701a85de910
Matthew Wampler-Doty <matthew.wampler.doty@gmail.com>
2016-04-12 07:45:33 -0600

    Random kernel stubs

--- Random/cuda/random.cu
+++ Random/cuda/random.cu
@@ -1,10 +1,33 @@
 #include "random.h"
+#include "warpspeed.h"
  
+__global__ void warpspeed_rand(uint32_t * random_data)
+{
+    // Get our global thread ID
+    int id = threadIdx.x + 
+              threadIdx.y * blockDim.x + 
+              threadIdx.z * blockDim.x * blockDim.y;
+    uint32_t temp;
+
+    temp = random_data[id];
+    temp += random_data[id + TAP1 - RANDOM_FRAME_SIZE];
+    temp += random_data[id + TAP2 - RANDOM_FRAME_SIZE];
+    temp += random_data[id + TAP3 - RANDOM_FRAME_SIZE];
+    temp += random_data[id + LENGTH - RANDOM_FRAME_SIZE];
+    random_data[id] = temp;
+}
+
 // CUDA kernel. Each thread takes care of one element of c
 __global__ void vecAdd(double *a, double *b, double *c, int n)
 {
+    // int id = blockIdx.x*blockDim.x + threadIdx.x;
+
     // Get our global thread ID
-    int id = blockIdx.x*blockDim.x+threadIdx.x;
+    int id = threadIdx.x + 
+             threadIdx.y * blockDim.x + 
+             threadIdx.z * blockDim.x * blockDim.y;
+
+    //int warpid = id / 32;
  
     // Make sure we do not go out of bounds
     if (id < n)
--- Random/cuda/random.h
+++ Random/cuda/random.h
@@ -2,7 +2,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+#include <stdint.h>
 
 // CUDA kernel. Each thread takes care of one element of c
+__global__ void warspeed_rand(uint32_t *random_numbers, int n, int rounds);
 __global__ void vecAdd(double *a, double *b, double *c, int n);
 int main( int argc, char* argv[] );
